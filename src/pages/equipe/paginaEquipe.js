73a09ec1a2d131db21b3b31001a17dda3283d5ee
diasmatheus05 <diasmatheus05@gmail.com>
2020-09-15 19:10:31 -0300

    Atualização na equipe

--- src/pages/equipe/paginaEquipe.js
+++ src/pages/equipe/paginaEquipe.js
@@ -8,7 +8,7 @@ import useNavBar from '../../hooks/useNavBar';
 import { withRouter } from 'react-router-dom'
 import { HashLink } from 'react-router-hash-link';
 
-import image_inicio from '../../assets/img/fotos/_MG_0002-Pano.jpg'
+// import image_inicio from '../../assets/img/fotos/_MG_0002-Pano.jpg'
 import image_scroll_icon_black from '../../assets/icons/scroll-icon-black.svg'
 
 import Equipe from './components/equipe'
@@ -20,7 +20,7 @@ const dados = [
     {
         "equipe": "2020",
         "capitao": {
-            "imagem": image_inicio,
+            "imagem": "https://upload.wikimedia.org/wikipedia/commons/f/f5/Steve_Jobs_Headshot_2010-CROP2.jpg",
             "sexo": "M",
             "nome": "Fulano de Tal" 
         },
@@ -29,27 +29,27 @@ const dados = [
                 "nome": "Administração",
                 "membros": [
                     {
-                        "imagem": image_inicio,
+                        "imagem": "https://static.wixstatic.com/media/d979eb_23bc3bead19d4b4aaabeb0e7e5c808e4~mv2_d_2976_3968_s_4_2.jpg/v1/crop/x_0,y_224,w_2976,h_2981/fill/w_160,h_160,al_c,q_80,usm_0.66_1.00_0.01/_MG_0908.webp",
                         "nome": "Matheus Felicetti",
                         "cargo": "Vice-Capitão"
                     },
                     {
-                        "imagem": image_inicio,
+                        "imagem": "https://static.wixstatic.com/media/d979eb_110f722593df4a3582ec8040eaee19c6~mv2_d_3049_4065_s_4_2.jpg/v1/crop/x_0,y_324,w_3049,h_3054/fill/w_160,h_160,al_c,q_80,usm_0.66_1.00_0.01/_MG_0916.webp",
                         "nome": "Matheus Felicetti",
                         "cargo": "Vice-Capitão"
                     },
                     {
-                        "imagem": image_inicio,
+                        "imagem": "https://static.wixstatic.com/media/d979eb_a0e947776e5c4f7c982af3f4e531ae00~mv2.png/v1/fill/w_160,h_160,al_c,q_85,usm_0.66_1.00_0.01/TESLED.webp",
                         "nome": "Matheus Felicetti",
                         "cargo": "Vice-Capitão"
                     },
                     {
-                        "imagem": image_inicio,
+                        "imagem": "https://static.wixstatic.com/media/d979eb_14d4352f1b14413db8b5b58982121895~mv2_d_2806_4209_s_4_2.jpg/v1/crop/x_0,y_351,w_2806,h_2806/fill/w_160,h_160,al_c,q_80,usm_0.66_1.00_0.01/_MG_0932.webp",
                         "nome": "Matheus Felicetti",
                         "cargo": "Vice-Capitão"
                     },
                     {
-                        "imagem": image_inicio,
+                        "imagem": "https://static.wixstatic.com/media/d979eb_e2107e5dc5a242d5a52ef9a29ce57530~mv2_d_3005_4007_s_4_2.jpg/v1/crop/x_0,y_169,w_3005,h_3010/fill/w_160,h_160,al_c,q_80,usm_0.66_1.00_0.01/_MG_0938.webp",
                         "nome": "Matheus Felicetti",
                         "cargo": "Vice-Capitão"
                     },
@@ -59,17 +59,17 @@ const dados = [
                 "nome": "Dinâmica",
                 "membros": [
                     {
-                        "imagem": image_inicio,
+                        "imagem": "https://static.wixstatic.com/media/d979eb_a0e947776e5c4f7c982af3f4e531ae00~mv2.png/v1/crop/x_350,y_0,w_900,h_900/fill/w_160,h_160,al_c,q_85,usm_0.66_1.00_0.01/TESLED.webp",
                         "nome": "Matheus Felicetti",
                         "cargo": "Vice-Capitão"
                     },
                     {
-                        "imagem": image_inicio,
+                        "imagem": "https://static.wixstatic.com/media/d979eb_24926a1c77eb4b90a24a12866b69cbad~mv2_d_3016_4021_s_4_2.jpg/v1/crop/x_0,y_170,w_3016,h_3020/fill/w_160,h_160,al_c,q_80,usm_0.66_1.00_0.01/_MG_0928.webp",
                         "nome": "Matheus Felicetti",
                         "cargo": "Vice-Capitão"
                     },
                     {
-                        "imagem": image_inicio,
+                        "imagem": "https://static.wixstatic.com/media/d979eb_a0e947776e5c4f7c982af3f4e531ae00~mv2.png/v1/crop/x_350,y_0,w_900,h_900/fill/w_160,h_160,al_c,q_85,usm_0.66_1.00_0.01/TESLED.webp",
                         "nome": "Matheus Felicetti",
                         "cargo": "Vice-Capitão"
                     },
